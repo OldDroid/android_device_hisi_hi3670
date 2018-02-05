fc062d0c78e1bcec839211094ce7564c8e09c054
Huawei-Dev <surdu_petru@yahoo.com>
2018-02-05 22:30:59 +0100

    add OmniClock

--- device.mk
+++ device.mk
@@ -74,3 +74,7 @@ PRODUCT_COPY_FILES += \
 # Telephony
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# OmniClock
+PRODUCT_PACKAGES += \
+    OmniClockOSS
